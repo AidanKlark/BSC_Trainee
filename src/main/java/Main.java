a421de7835dda271caae9ae98e998f126ef51961
AidanKlark <85440133+AidanKlark@users.noreply.github.com>
2021-10-24 20:40:46 +0300

    Обновил README

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
